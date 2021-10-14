2ad61856f3c311aabd7d6f6c99e665600868952c
Ray <aes6669ray@gmail.com>
2021-10-14 15:49:10 +0800

    init

---
+++ 10_14.r
@@ -0,0 +1,22 @@
+# a <- c(seq(0, 1, 0.1), seq(2, 10))
+
+# t <- 500
+# x <- pnorm(t)
+# g=sum(.5<x^2 & x^2<1.2)/t
+# print(g)
+
+par(mfrow=c(3,1))
+
+x= (30:-30)/ 10 
+y= (0:100)/ 100
+
+
+plot(y,qnorm(y),type="l")
+plot(x,dnorm(x),type="l") 
+plot(x,pnorm(x),type="l")
+
+#x <- rf(100000, df1 = 3, df2 = 25)
+#hist(x, breaks = 'Scott', freq = FALSE, xlim = c(0,10))
+
+# a= seq(0,10,0.001)
+# plot(a, df(a,df1=3,df2=25),type="l")
--- test.r
+++ test.r
@@ -4,3 +4,10 @@ barplot(h,
     names.arg = M, xlab = "X", ylab = "Y",
     col = "#00cec9", main = "Chart", border = "#fdc46e"
 )
+
+# attach(mtcars)
+# par(mfrow=c(2,2))
+# plot(wt,mpg, main="Scatterplot of wt vs. mpg")
+# plot(wt,disp, main="Scatterplot of wt vs disp")
+# hist(wt, main="Histogram of wt")
+# boxplot(wt, main="Boxplot of wt")
