3bc62835c14348cca96f2f1e5656037fc61b225f
Ray <aes6669ray@gmail.com>
2021-10-21 16:27:01 +0800

    init

--- 10_21.r
+++ 10_21.r
@@ -1,4 +1,5 @@
-hu <- read.table("housing.txt", header = TRUE, row.names = 1)
+
+#hu <- read.table("housing.txt", header = TRUE, row.names = 1)
 
 # std_apt <- (hu[,1]-mean(hu[,1]))/sd(hu[,1])
 # std_hou <- (hu[,2]-mean(hu[,2]))/sd(hu[,2])
@@ -8,18 +9,32 @@ hu <- read.table("housing.txt", header = TRUE, row.names = 1)
 # print(sapply(dta,sd))
 
 
-test <- function(x){
-    q<-c()
-    for (i in c(1:x)){
-    a <- seq(1:i)
-    k <- append(q, a)
-    q<-k
+# test <- function(x){
+#     q<-c()
+#     for (i in c(1:x)){
+#     a <- seq(1:i)
+#     k <- append(q, a)
+#     q<-k
+#     }
+#     return(q)
+# }
+
+
+# a<-test(8)
+#print(a)
+
+simcor <- function(ncor, nnor, rho){
+    if (ncor < 1 || nnor < 2 || rho < -1 || rho > 1) return(NA)
+    library(mvtnorm)
+    vm <- matrix(c(1,rho,rho,1),2,2)
+    result <- NULL
+    for (i in c(1:ncor)){
+        norv <- rmvnorm(nnor,mean = c(0,0), sigma= vm)
+        sc <- cor(norv)[1,2]
+        result <- c(result,sc)
     }
-    return(q)
+    return(result)
 }
 
-
-a<-test(8)
-print(a)
-
-
+j<- simcor(ncor=5,nnor=15,rho=0)
+print(j)
