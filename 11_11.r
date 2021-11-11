b1d9863412f85d086c3683fcd96918b7847e70a2
Ray <aes6669ray@gmail.com>
2021-11-11 15:10:44 +0800

    init

--- 11_11.r
+++ 11_11.r
@@ -1,4 +1,18 @@
 burger = read.table(r"(C:\Users\aes6669ray\Desktop\R Test\data\burger.txt)", header = TRUE, row.names = 1)
+library(aplpack)
+#pairs(burger,col = "red",gap  = 0 ,xaxt = "n", yaxt =  "n", panel = panel.smooth, col.smooth =  "blue")
 
-pairs(burger,col = "red",gap  = 0 ,xaxt = "n", yaxt =  "n", panel = panel.smooth, col.smooth =  "blue")
+#bagplot.pairs(burger[,4:7],gap=0,factor = 4,pch = 16,col.baghull = "green",main = "")
 
+#library(graphics)
+#coplot(Cal ~ Sodium |Fat, data=burger,rows=1,pch=16,number = 3,col="red",bar.bg = c(num="blue",fac=gray(0.95)))
+
+#palette(rainbow(7))
+#burs = burger[order(burger$Cal),]
+#stars(burs,len=1,key.loc=c(12.5,2),labels=row.names(burs),draw.segments = TRUE)
+
+library(MASS)
+op=par(cex.axis=1.4)
+ir=rbind(iris3[,,1],iris3[,,2],iris3[,,3])
+#print(ir)
+parcoord(log(ir))
